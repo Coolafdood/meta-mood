a7ee7acadaf8362380008649c0d36d2e4d842df4
Jw@h3r <jawahir.abdulbari@gmail.com>
2026-02-28 13:17:35 +0000

    Added static files, update base & index templates

--- moodtracker/settings.py
+++ moodtracker/settings.py
@@ -131,8 +131,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
